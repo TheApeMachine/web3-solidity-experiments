c805d1012d879b2eec6f7f5527da53f651bc79b4
Daniel Owen van Dommelen <9572060+TheApeMachine@users.noreply.github.com>
2022-11-07 23:04:07 +0100

    Some fixes

--- main.go
+++ main.go
@@ -7,38 +7,28 @@ import (
 	"github.com/theapemachine/wrkspc/sockpuppet"
 )
 
-type Manager struct{}
-
-func NewManager() *Manager {
-	return &Manager{}
-}
-
-func (manager *Manager) Manage() {}
-
 func main() {
 	client := sockpuppet.NewEthClient(
 		"http://127.0.0.1:7545",
 		"fd4eef6dec5575cc78f3f14d4b749094f8b88ad7883caaa8d1d24e9a01e3732d",
 	)
 
+	// Deploy the smart contract function.
 	address, tx, instance, err := punchface.DeployPunchface(
 		client.Auth(), client.Conn(),
 	)
 	errnie.Handles(err)
 	errnie.Logs(address.Hex())
 
-	_, _ = instance, tx
 	errnie.Logs("instance", instance)
 	errnie.Logs("tx", tx.Hash().Hex())
 
-	//creating api object to intract with smart contract function
+	// Get a handle on the smart contract function.
 	conn, err := punchface.NewPunchface(
 		common.HexToAddress(address.Hex()), client.Conn(),
 	)
 	errnie.Handles(err)
 
-	srv := sockpuppet.NewContract(
-		punchface.NewNotary(conn),
-	)
-	srv.Up("80")
+	// Setup the smart contract server.
+	sockpuppet.NewContract(punchface.NewNotary(conn)).Up("80")
 }
